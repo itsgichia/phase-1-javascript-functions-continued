9431da93dac913dac5849a452a1b85998d71ea16
itsgichia <gichiamuiruri@gmail.com>
2023-12-20 20:21:26 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondayWork function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example 
+  console.log(saturdayFun());  // Output: "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("paint"));  // Output: "This Saturday, I want to paint!"
+  
+  console.log(mondayWork());  // Output: "This Monday, I will go to the office."
+  console.log(mondayWork("code"));  // Output: "This Monday, I will code."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer"));
+  // Output: "You are !!!a dedicated programmer!!!"
+  
